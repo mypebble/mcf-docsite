ec55498b233e503f720b8132921efabd20d7a4e1
tpict <tom@tompicton.co.uk>
2018-09-17 08:39:07 +0100

    Update CircleCI config

--- send-to-s3.sh
+++ send-to-s3.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ## Based on http://benlopatin.com/deploying-static-sites-circle-ci/
-aws s3 sync $SYNC_DIR s3://$S3_BUCKET/$S3_DIR --delete --acl public-read --cache-control max-age=120
+
+AWS_ACCESS_KEY_ID=$NEW_AWS_ID AWS_SECRET_ACCESS_KEY=$NEW_AWS_SECRET aws s3 sync $SYNC_DIR s3://$S3_BUCKET --delete --acl public-read --cache-control max-age=120
