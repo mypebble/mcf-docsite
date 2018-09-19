4058a35e6411741f242248b2f927b91cf312fc1a
Tom Picton <tom@tompicton.co.uk>
2018-09-19 08:59:45 +0100

    Use build instead of deploy for script execution

--- send-to-s3.sh
+++ send-to-s3.sh
@@ -2,4 +2,4 @@
 
 ## Based on http://benlopatin.com/deploying-static-sites-circle-ci/
 
-AWS_ACCESS_KEY_ID=$NEW_AWS_ID AWS_SECRET_ACCESS_KEY=$NEW_AWS_SECRET aws s3 sync $SYNC_DIR s3://$S3_BUCKET --delete --acl public-read --cache-control max-age=120
+AWS_ACCESS_KEY_ID=$NEW_AWS_ID AWS_SECRET_ACCESS_KEY=$NEW_AWS_SECRET aws s3 sync output s3://$S3_BUCKET  --acl public-read --cache-control max-age=120
